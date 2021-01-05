1202a8268b33fe8d05f1fa92cfbbc57cee8cb46f
Disha-Hati <70029959+Disha-Hati@users.noreply.github.com>
2021-01-05 22:55:03 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Travel Recommendation</h1>
-      <h2>Get information about your dream destination</h2>
+      <h1>Let's Travel ✈️</h1>
+      <h2>
+        🎑Checkout my Travel Reccomendation.Select the type of place to get
+        started.🎑
+      </h2>
+      <button className="btn">Hills</button>
+      <button className="btn">Beach</button>
+      <button className="btn">Forest</button>
     </div>
   );
 }
